021dd972926002e18cc3c575f6d2f539d0717103
Arthur Osmokiesku <arthur.osmokiesku@gmail.com>
2020-11-15 22:24:06 +0200

    Fixed bug #8 about migrations search in MongoDatabaseStateChecker

--- MongoDBMigrations/MongoDatabaseStateChecker.cs
+++ MongoDBMigrations/MongoDatabaseStateChecker.cs
@@ -1,29 +1,36 @@
-﻿using MongoDB.Driver;
+﻿using System.Reflection;
+using MongoDB.Driver;
 using MongoDBMigrations.Document;
 
 namespace MongoDBMigrations
 {
     public static class MongoDatabaseStateChecker
     {
-        public static void ThrowIfDatabaseOutdated(string connectionString, string databaseName, MongoEmulationEnum emulation = MongoEmulationEnum.None)
+        public static void ThrowIfDatabaseOutdated(string connectionString, string databaseName, Assembly migrationAssambly = null, MongoEmulationEnum emulation = MongoEmulationEnum.None)
         {
-            var (dbVersion, availableVersion) = GetCurrentVersions(connectionString, databaseName, emulation);
+            var (dbVersion, availableVersion) = GetCurrentVersions(connectionString, databaseName, migrationAssambly, emulation);
             if (dbVersion < availableVersion)
                 throw new DatabaseOutdatedExcetion(dbVersion, availableVersion);
         }
 
-        public static bool IsDatabaseOutdated(string connectionString, string databaseName, MongoEmulationEnum emulation = MongoEmulationEnum.None)
+        public static bool IsDatabaseOutdated(string connectionString, string databaseName, Assembly migrationAssambly = null, MongoEmulationEnum emulation = MongoEmulationEnum.None)
         {
-            var (dbVersion, availableVersion) = GetCurrentVersions(connectionString, databaseName, emulation);
+            var (dbVersion, availableVersion) = GetCurrentVersions(connectionString, databaseName, migrationAssambly, emulation);
             return availableVersion > dbVersion;
         }
 
-        private static (Version dbVersion, Version availableVersion) GetCurrentVersions(string connectionString, string databaseName, MongoEmulationEnum emulation)
+        private static (Version dbVersion, Version availableVersion) GetCurrentVersions(string connectionString, string databaseName, Assembly migrationAssambly, MongoEmulationEnum emulation)
         {
             var locator = new MigrationManager();
+            if(migrationAssambly != null)
+            {
+                locator.SetAssembly(migrationAssambly);
+            }
             var highestAvailableVersion = locator.GetNewestLocalVersion();
+
             var dbStatus = new DatabaseManager(new MongoClient(connectionString).GetDatabase(databaseName), emulation);
             var currectDbVersion = dbStatus.GetVersion();
+
             return (currectDbVersion, highestAvailableVersion);
         }
     }
