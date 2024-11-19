41e575fddcbf277257c8eca8f0397aadee0a13a6
Ruxo Zheng <ruxo@users.noreply.github.com>
2024-11-19 07:21:48 +0700

    Update to .NET 9. Replace Mongo2Go with MongoSandbox

--- MongoDBMigrations.SmokeTests/MongoDaemon.cs
+++ MongoDBMigrations.SmokeTests/MongoDaemon.cs
@@ -1,44 +1,30 @@
 ﻿using System;
-using System.Runtime.InteropServices;
-using Microsoft.Extensions.Configuration;
-using Mongo2Go;
+using System.Diagnostics;
+using System.Threading;
+using MongoSandbox;
 
 namespace MongoDBMigrations.SmokeTests;
 
 public static class MongoDaemon
 {
-    public sealed class ConnectionInfo(MongoDbRunner runner) : IDisposable
+    public sealed class ConnectionInfo : IDisposable
     {
-        public string ConnectionString => runner.ConnectionString;
+        public string ConnectionString => Server.ConnectionString;
         public required string DatabaseName { get; init; }
 
-        public void Dispose() {
-            runner.Dispose();
-        }
+        public void Dispose() { }
     }
 
-    // ReSharper disable InconsistentNaming
-    sealed class AppConfig
-    {
-        public required string databaseName { get; init; }
-        public bool isLocal { get; init; }
-        public string? dbFolder { get; init; }
-    }
-    // ReSharper restore InconsistentNaming
+    static int dbCounter;
+    public static ConnectionInfo Prepare()
+        => new(){ DatabaseName = $"test{Interlocked.Increment(ref dbCounter)}"};
 
-    public static ConnectionInfo Prepare() {
-        var dbFolder = Configuration.Value.isLocal ? Configuration.Value.dbFolder : null;
-        return new (MongoDbRunner.Start(dbFolder)){ DatabaseName = Configuration.Value.databaseName };
-    }
+    static readonly MongoRunnerOptions MongoOptions = new() {
+        UseSingleNodeReplicaSet = true,
+        StandardOuputLogger = line => Trace.WriteLine($"| {line}"),
+        StandardErrorLogger = line => Trace.WriteLine($"ERR: {line}"),
 
-    static readonly Lazy<AppConfig> Configuration = new(() => {
-        //This class is only for test/dev purposes. And supports only windows and osx platform as a development environment.
-        var section = RuntimeInformation.IsOSPlatform(OSPlatform.Windows) ? "windows" : "osx";
-
-        return new ConfigurationBuilder()
-              .AddJsonFile("local.json")
-              .Build()
-              .GetRequiredSection(section)
-              .Get<AppConfig>()!;
-    });
+        KillMongoProcessesWhenCurrentProcessExits = true
+    };
+    static readonly IMongoRunner Server = MongoRunner.Run(MongoOptions);
 }
