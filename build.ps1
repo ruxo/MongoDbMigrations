5de144c987c5ec5e9c5e135739d9cfb425793afc
Ruxo Zheng <ruxo@users.noreply.github.com>
2024-11-19 07:44:22 +0700

    Remove SLNX. Add build.ps1

---
+++ build.ps1
@@ -0,0 +1,20 @@
+$destination = $args[0]
+
+function build {
+    param ($path)
+    Write-Output "Target: $path"
+    Push-Location $path
+    Remove-Item ./bin/Release/*.nupkg
+    dotnet pack -c Release
+    $file = Get-Item ./bin/Release/*.nupkg
+    Write-Output "Build $($file.Name)"
+    Move-Item ./bin/Release/*.nupkg $destination
+    Pop-Location
+}
+
+Push-Location ./src
+
+build .\MongoDBMigrationsRZ
+build .\MongoDBMigrations.SshTunnel
+
+Pop-Location
